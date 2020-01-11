0b9091e51e97b098f9fb9fff8d88a5a2bf00b3a5
JEVLOGIN <jev.login@users.noreply.github.com>
2020-01-11 18:58:56 +0300

    Create constructor Point(int x, int y, char sym)

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -8,9 +8,21 @@ namespace Snake
 {
     class Point
     {
-        public int x;
-        public int y;
-        public char sym;
+        private int x;
+        private int y;
+        private char sym;
+
+        public Point()
+        {
+
+        }
+
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
 
         public void Draw()
         {
